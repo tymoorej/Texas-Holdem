b061830841c96df6aba0c9bbb1c5ce0557ae4742
jhryniw <jhryniw@gmail.com>
2018-11-09 16:56:03 -0700

    reformat

--- database.py
+++ database.py
@@ -1,19 +1,24 @@
-import pyodbc 
+import pyodbc
+
 
 def setupDB():
     ServerName = "den1.mssql5.gear.host"
     MSQLDatabase = 'tymooredb'
     username = 'tymooredb'
     password = '3I%sZCr!QcCQiPIn%06F'
-    connection = pyodbc.connect('Driver=SQL Server;Server={};Database={};uid={};pwd={}'.format(ServerName,MSQLDatabase,username,password))
-    
+    connection = pyodbc.connect(
+        'Driver=SQL Server;Server={};Database={};uid={};pwd={}'.format(
+            ServerName, MSQLDatabase, username, password))
+
     return connection
+
 
 def finishDB(connection, cursor):
     connection.commit()
     cursor.close()
     connection.close()
- 
+
+
 def getRows(table_name):
     connection = setupDB()
     cursor = connection.cursor()
@@ -21,6 +26,7 @@ def getRows(table_name):
     results = cursor.fetchall()
     finishDB(connection, cursor)
     return results
+
 
 def main():
     print(getRows('PokerStandings'))
